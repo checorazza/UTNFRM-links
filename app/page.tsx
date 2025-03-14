40dd29f49c176f6e349331c68fd0237c1c1cfae0
Juan Pablo Biondo <jupbiondo@gmail.com>
2025-03-14 15:04:34 -0300

    update page to use LinkGroup

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import Image from "next/image";
-
-import LinkList from "../components/LinkList.js";
-import LinkItem from "../components/LinkItem.js";
-import Divider from "../components/Divider.js";
-import { principal, apuntes, horarios, grupos } from "../data/link.js";
+import LinkGroup from "../components/LinkGroup.jsx";
+import { linksSections } from "../data/link.js";
 
 export default function Home() {
   return (
     <main className="bg-gradient-to-b text-center items-center justify-center min-h-screen font-sans">
-      <section className="m-8 p-8 rounded-3xl shadow-md w-full max-w-md mx-auto bg-white">
+      <section className="m-8 px-8 py-12 rounded-3xl shadow-md w-full max-w-md mx-auto bg-white">
         <header className="mx-auto">
           <Image
             src={"/logo-utn.png"}
@@ -22,16 +19,17 @@ export default function Home() {
           </h1>
           <p className="text-gray-600">Links Útiles</p>
         </header>
-        <h2 className="text-3xl font-bold tracking-tight text-blue-500 mb-5">
-          Links Útiles
-        </h2>
-        <LinkList linkitems={principal} />
-        <Divider text="Apuntes"></Divider>
-        <LinkList linkitems={apuntes} />
-        <Divider text="Horarios"></Divider>
-        <LinkList linkitems={horarios} />
-        <Divider text="Grupos"></Divider>
-        <LinkList linkitems={grupos} />
+        <hr className="my-8 border-gray-300" />
+        <div className="flex flex-col gap-4">
+          {linksSections.map((linkSection, index) => (
+            <LinkGroup
+              key={index}
+              title={linkSection.title}
+              items={linkSection.items}
+              icon={linkSection.icon}
+            />
+          ))}
+        </div>
       </section>
     </main>
   );
