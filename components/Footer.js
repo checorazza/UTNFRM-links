0b662326fd591a9a5860086194e083f48196a538
checorazza <ceciicorazza@gmail.com>
2025-03-14 17:16:39 -0300

    add maestro jpbiondo to the footer because wtf this looks so good

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,18 @@
 const Footer = () => {
   return (
-    <footer className="relative flex flex-col py-8 items-center max-w-md mx-auto">
+    <footer className="relative flex flex-col py-8 items-center max-w-full mx-auto">
       <div className="flex flex-col items-center w-full">
         <div className="flex-grow border-t border-gray-400 w-full mb-6"></div>
         <div className="flex flex-col items-center gap-4">
           <p className="text-gray-600 text-base">
-            © {new Date().getFullYear()} checorazza
+            © {new Date().getFullYear()}{" "}
+            <span className="text-blue-500 hover:text-blue-700">
+              <a href="https://instagram.com/checorazza"> checorazza </a>
+            </span>
+            - Diseño del maestro
+            <span className="text-blue-500 hover:text-blue-700">
+              <a href="https://github.com/jpbiondo"> jpbiondo</a>
+            </span>
           </p>
           <div className="flex gap-6">
             <a
